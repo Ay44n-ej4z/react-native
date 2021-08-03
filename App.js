b061327559b217132c6e9eb3bbfb3467e611cace
Ay44n-ej4z <ayaan.ejaz185@gmail.com>
2021-08-03 13:06:38 +0530

    start_an_app

--- App.js
+++ App.js
@@ -1,11 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import pic from "./assets/favicon.png"
 
 export default function App() {
+  const menus = [
+    {
+      _id: "5f1c2cf788c5e8bfc63e85d4",
+      title: "Electronics",
+      parent: "/",
+      category: "/electronics"
+    },
+    {
+      _id: "5f1c2d0e88c5e8bfc63e85d5",
+      title: "Headphones",
+      parent: "/electronics",
+      category: "/electronics/headphones"
+    },
+    {
+      _id: "5f1c2d1888c5e8bfc63e85d6",
+      title: "Television",
+      parent: "/electronics",
+      category: "/electronics/television"
+    },
+  {
+      _id: "5f1c2d2088c5e8bfc63e85d7",
+      title: "Mobile",
+      parent: "/electronics",
+      category: "/electronics/mobiles"
+    },
+    {
+      _id: "5f1c2d4288c5e8bfc63e85d8",
+      title: "Samsung",
+      parent: "/electronics/mobiles",
+      category: "/electronics/mobiles/samsung"
+    },
+    {
+      _id: "5f1c2d4288c5e8bfc63e85d8",
+      title: "Redmi",
+      parent: "/electronics/mobiles",
+      category: "/electronics/mobiles/redmi"
+    },
+  {
+      _id: "5f1c2cf788c5e8bfc63e85d4",
+      title: "Fashion",
+      parent: "/",
+      category: "/fashion"
+    },
+    {
+      _id: "5f1c2cf788c5e8bfc63e85d4",
+      title: "Men",
+      parent: "/fashion",
+      category: "/fashion/men"
+    },
+    {
+      _id: "5f1c2cf788c5e8bfc63e85d4",
+      title: "Women",
+      parent: "/fashion",
+      category: "/fashion/women"
+    }
+    
+  ];
+   
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source = {pic}  />
+      <Text >
+        {menus.map(menu => 
+          <Text >
+            {menu.parent == "/electronics"}
+          </Text>
+        )}
+
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +81,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#191919',
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'white'
   },
 });
